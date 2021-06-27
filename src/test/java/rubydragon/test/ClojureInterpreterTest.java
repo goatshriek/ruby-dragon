4d611d9a23fe696bd71fa3e92dc5f3fadf586a0b
Joel Anderson <joelanderson333@gmail.com>
2021-06-27 07:14:28 -0400

    add basic unit test

---
+++ src/test/java/rubydragon/test/ClojureInterpreterTest.java
@@ -0,0 +1,12 @@
+package rubydragon.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ClojureInterpreterTest {
+	@Test
+	public void testFailure() throws Exception {
+		assertEquals("test1", "test1");
+	}
+}
