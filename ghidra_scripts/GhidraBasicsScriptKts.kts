effd5bc21ed1bfd7781c5085df80c1b3ae6f9217
Joel Anderson <joelanderson333@gmail.com>
2022-02-04 10:47:50 -0500

    start completing ghidra basics in kotlin

--- ghidra_scripts/GhidraBasicsScriptKts.kts
+++ ghidra_scripts/GhidraBasicsScriptKts.kts
@@ -8,7 +8,19 @@ val languageId = currentProgram.getLanguageID()
 val compilerSpecId = currentProgram.getCompilerSpec().getCompilerSpecID()
 
 // printing out some basic program information
-// remember to use the script.println function
+// remember to use the script.print functions
 script.println("Program Info:")
-script.println(programName + " " + languageId + " (" + compilerSpecId + ")")
+script.println("$programName $languageId ($compilerSpecId)\n")
 script.println()
+
+// get info about the current program's memory layout
+script.println("Memory Layout:")
+script.printf("Imagebase: 0x%x", currentProgram.getImageBase().getOffset())
+currentProgram.getMemory().getBlocks().forEach {
+  script.println("${it.getName()} [start: 0x${it.getStart()}, end: 0x${it.getEnd()}]")
+}
+script.println()
+
+// get the current program's function names
+script.println("Function List:")
+
