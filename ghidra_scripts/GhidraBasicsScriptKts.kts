5baae8467d460860873ebab13bef930b5ad338e5
Joel Anderson <joelanderson333@gmail.com>
2022-02-04 10:13:50 -0500

    fix output detection

--- ghidra_scripts/GhidraBasicsScriptKts.kts
+++ ghidra_scripts/GhidraBasicsScriptKts.kts
@@ -3,5 +3,12 @@
 
 // of course, standard variable assignments and type inference work as expected
 val programName = currentProgram.getName()
+val creationDate = currentProgram.getCreationDate()
+val languageId = currentProgram.getLanguageID()
+val compilerSpecId = currentProgram.getCompilerSpec().getCompilerSpecID()
 
-script.println(programName)
+// printing out some basic program information
+// remember to use the script.println function
+script.println("Program Info:")
+script.println(programName + " " + languageId + " (" + compilerSpecId + ")")
+script.println()
