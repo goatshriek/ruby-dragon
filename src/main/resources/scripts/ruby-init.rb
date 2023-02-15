61e69574ebbe3276619e0363b1d087dfe5397a5f
Joel <joelanderson333@gmail.com>
2023-02-15 18:50:31 -0500

    update for ghidra 10.2.3 (#30)
    Updates JRuby, Groovy, and Kotlin to newer versions, and updates the CI
    workflow to use Ghidra 10.2.3.
    
    Some updates have also been made to the Ruby IRB script. JRuby itself has
    been kept on the 9.3 minor version, as 9.4 continues to have problems
    with IRB sessions on Windows machines.

--- src/main/resources/scripts/ruby-init.rb
+++ src/main/resources/scripts/ruby-init.rb
@@ -3,20 +3,19 @@ require 'irb/completion'
 
 # allow java-like package names, and import irb and completions
 def ghidra
-	Java::ghidra
+  Java::ghidra
 end
 
 #ghidra classes (mostly) have good toString values meant for jython, so just use them vs useless java class inspect
 class java::lang::Object
-	def inspect(*args)
-		return self.class.name.start_with?("Java::Ghidra") ? "#<#{to_string}>" : super(*args)
-	end
+  def inspect(*args)
+    return self.class.name.start_with?("Java::Ghidra") ? "#<#{to_string}>" : super(*args)
+  end
 end
 
 # Now configure IRB
-
-# JRuby-9.3 ships with a slightly broken irb, and we want to modify it anyway, so look at
-# https://github.com/ruby/irb/blob/master/lib/irb/init.rb#L367 where this was copied from
+# this was copied from
+# https://github.com/ruby/irb/blob/ed9e435a6beba805b3a8b453369cc6117ec7e377/lib/irb/init.rb#L398
 module IRB
   # enumerate possible rc-file base name generators
   def IRB.rc_file_generators
@@ -24,24 +23,24 @@ module IRB
       yield proc{|rc| rc == "rc" ? irbrc : irbrc+rc}
     end
     if xdg_config_home = ENV["XDG_CONFIG_HOME"]
-      irb_home = File.join(xdg_config_home, "ghidra")
-      unless File.exist? irb_home
-        require 'fileutils'
-        FileUtils.mkdir_p irb_home
+      irb_home = File.join(xdg_config_home, "irb")
+      if File.directory?(irb_home)
+        yield proc{|rc| irb_home + "/irb#{rc}"}
       end
-      yield proc{|rc| irb_home + "/irb#{rc}"}
     end
     if home = ENV["HOME"]
       yield proc{|rc| home+"/.irb#{rc}"}
     end
+
     # TODO: I'd love to do this, but this isn't available at startup
-#    current_dir = project_root_folder.project_locator.project_dir.to_s
-	current_dir = Dir.pwd
-#    yield proc{|rc| current_dir+"/.config/irb/irb#{rc}"}
+#   current_dir = project_root_folder.project_locator.project_dir.to_s
+    current_dir = Dir.pwd
+
+#   yield proc{|rc| current_dir+"/.config/irb/irb#{rc}"}
     yield proc{|rc| current_dir+"/.irb#{rc}"}
-#    yield proc{|rc| current_dir+"/irb#{rc.sub(/\A_?/, '.')}"}
-#    yield proc{|rc| current_dir+"/_irb#{rc}"}
-#    yield proc{|rc| current_dir+"/$irb#{rc}"}
+#   yield proc{|rc| current_dir+"/irb#{rc.sub(/\A_?/, '.')}"}
+#   yield proc{|rc| current_dir+"/_irb#{rc}"}
+#   yield proc{|rc| current_dir+"/$irb#{rc}"}
   end
 end
 
